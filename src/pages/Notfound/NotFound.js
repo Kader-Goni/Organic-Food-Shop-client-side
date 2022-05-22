2935e6a3971c2c204ea61304f2f3c92f9fb8ad1c
Kader-Goni <kadergoni35@gmail.com>
2022-05-23 00:00:17 +0600

    404 page added

---
+++ src/pages/Notfound/NotFound.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import NotFound from '../../images/NotFound.jpg'
+
+const Notfound = () => {
+    return (
+        <div className='text-danger mt-5'>
+            <div className="w-50 mx-auto bg-danger bg-opacity-25 rounded-3 p-3">
+                <h1>Opps... Page NOT FOUND...</h1>
+                <h1>404 errors...</h1>
+            </div>
+            <div className="p-5 margin-top" data-aos="zoom-in-up" data-aos-duration="800">
+                <img src={NotFound} className='img-fluid' alt="" />
+            </div>
+        </div>
+    );
+};
+
+export default Notfound;
